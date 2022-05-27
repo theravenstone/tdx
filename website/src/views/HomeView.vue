e16bc468141ef7ea17b0e913cc90305f2af101a1
theravenstone <theravenstonepsn@gmail.com>
2022-05-27 11:29:03 +0200

    .

--- website/src/views/HomeView.vue
+++ website/src/views/HomeView.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home mt-3">
+    <ToDoList></ToDoList>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import ToDoList from '@/components/ToDoList.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    ToDoList,
   }
 }
 </script>
