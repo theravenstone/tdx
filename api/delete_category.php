ea613e130f912257a7b4991fcd229d71ca8ce2a7
theravenstone <theravenstonepsn@gmail.com>
2022-05-30 19:42:56 +0200

    .

--- api/delete_category.php
+++ api/delete_category.php
@@ -6,13 +6,7 @@ $database = 'todo_app';
 $db = new PDO("mysql:host=127.0.0.1", $username, $password);
 $db->exec('use '. $database);
 
-$myparams = file_get_contents('php://input');
-
-$myparams = json_decode($myparams, true);
-
-
 http_response_code(200);
-
 
 $stmt = $db->prepare('
 DELETE FROM categories WHERE id = :id;
--- api/get_categories.php
+++ api/get_categories.php
@@ -6,10 +6,6 @@ $database = 'todo_app';
 $db = new PDO("mysql:host=127.0.0.1", $username, $password);
 $db->exec('use '. $database);
 
-$myparams = file_get_contents('php://input');
-
-$myparams = json_decode($myparams, true);
-
 
 http_response_code(200);
 
